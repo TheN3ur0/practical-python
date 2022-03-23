bda96d3cfdd2090e1fb98082a33e739a741636b3
Onur <rushxps3@gmail.com>
2022-03-23 20:24:38 +0100

    Changes

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
+# !/usr/bin/env python3
 # bounce.py
 #
 # Exercise 1.5
+
+
+height = 100 #Meters 
+amount = 0
+
+x = 9
+
+for i in range(x+1):
+    amount = amount + 1
+    height = height * 0.60
+    print(amount, round(height, 4))
