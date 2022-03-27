73d6db86ae8d5dd9a059c4ec39c70477fa4e7a39
Onur <rushxps3@gmail.com>
2022-03-27 23:35:51 +0200

    Updates

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,43 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys
+
+def portfolio_cost(filename):
+
+    with open (filename, 'rt') as f:
+
+        rows = csv.reader(f)
+
+        headers = next(rows)
+
+        totalRow = 0
+        sumTotal = 0
+
+        for row in rows:
+
+            try:
+                totalRow = int(row[1]) * float(row[2])
+
+                sumTotal = sumTotal + totalRow
+            
+            except ValueError:
+                print("Couldn't parse", line)
+    
+        return round(sumTotal, 2)
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+
+print("Total cost is", cost)
+       
+
+
+
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,74 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+from pprint import pprint
+
+
+def read_portfolio(filename):
+    
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+          
+            holding = {
+                    "name":row[0],
+                    "shares":int(row[1]),
+                    "price":float(row[2])
+                }
+
+            portfolio.append(holding)
+
+        return portfolio
+
+
+def read_prices(filename):
+
+    d = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        
+        try:
+            for row in rows:
+
+                d[row[0]] = float(row[1])
+
+        except IndexError:
+            row = None
+        
+
+        return d
+
+
+def check_unique():
+    
+    stockList = []
+    uniqueStocks = []
+
+    portfolio = read_portfolio('Data/portfolio.csv')
+    
+    for x in portfolio:
+
+        stockList.append(x['name'])
+        stockNames = set(stockList)
+    
+    for y in stockNames:
+        uniqueStocks.append(y)
+
+    return uniqueStocks
+
+#Stocknames, met de prijs en aantal
+
+#Huidige prijs van aandeel van stocks die in de portfolio zitten
+prices = read_prices('Data/prices.csv')
+
+#for y in prices:
+
+uniqueStocks = check_unique()
